5e8beb39b251bff094c2865cb8fc1b9b75ac6996
venkat1036 <vtu27600@veltech.edu.in>
2026-08-21 09:59:36 +0530

    Create session 3.java

---
+++ task 5/session 3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Find maximum element - O(n)
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+
+        sc.close();
+    }
+}
+
+Output:
+5
+10
+25
+7
+40
+15
+Maximum: 40
+
+=== Code Execution Successful ===
